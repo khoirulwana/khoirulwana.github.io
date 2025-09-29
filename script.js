ccabbb35883ed620515db3c611d2b202eb87e2dd
khoirul.wana <khoirul.wana@gmail.com>
2025-09-29 10:40:18 +0700

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const headerEl = document.getElementById("header");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 500) {
+  if (window.scrollY > 100) {
     // scroll lewat 50px
     headerEl.classList.add("scrolled");
   } else {
